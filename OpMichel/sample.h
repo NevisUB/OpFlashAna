b621dd3e947ad97c60b28961f2b8b3da0f5055e9
David <devish.c@gmail.com>
2015-08-29 15:18:44 -0500

    adding OpMichel repo for optical michel reconstruction -- David Caratelli

---
+++ OpMichel/sample.h
@@ -0,0 +1,36 @@
+/**
+ * \file sample.h
+ *
+ * \ingroup OpMichel
+ * 
+ * \brief Class def header for a class sample
+ *
+ * @author david
+ */
+
+/** \addtogroup OpMichel
+
+    @{*/
+#ifndef OPFLASHANA_OPMICHEL_SAMPLE_H
+#define OPFLASHANA_OPMICHEL_SAMPLE_H
+
+/**
+   \class sample
+   User defined class OpMichel ... these comments are used to generate
+   doxygen documentation!
+ */
+class sample{
+
+public:
+
+  /// Default constructor
+  sample(){}
+
+  /// Default destructor
+  virtual ~sample(){};
+
+};
+
+#endif
+/** @} */ // end of doxygen group 
+
